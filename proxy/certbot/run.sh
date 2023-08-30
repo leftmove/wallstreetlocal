d45aa29b3f38d54f5cfc458b338b391496b55d3e
bruhbruhroblox <100anonyo@gmail.com>
2023-08-30 13:11:04 -0400

    conf nginx

--- proxy/certbot/run.sh
+++ proxy/certbot/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+certonly --webroot --webroot-path /var/www/certbot/ -d content.wallstreetlocal.com -m zipped@wallstreetlocal.com --non-interactive --agree-tos
+
 if [ -d "/etc/letsencrypt/live" ]; then
     echo "Certificates Found, Updating NGINX Configuration for Port 443..."
     cp nginx443.conf /proxyconf
