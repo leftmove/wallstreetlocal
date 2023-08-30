db0b6204d75a86d51735737315968e2786502c63
bruhbruhroblox <100anonyo@gmail.com>
2023-08-30 12:47:37 -0400

    conf nginx

---
+++ proxy/certbot/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if /etc/letsencrypt/live directory exists
+if [ -d "/etc/letsencrypt/live" ]; then
+    echo "Certificates Found, Updating NGINX Configuration for Port 443..."
+    cp nginx443.conf /proxyconf
+    echo "NGINX Configuration Updated"
+else
+    echo "Certificates Found, Updating NGINX Configuration for Port 80..."
+    cp nginx80.conf /proxyconf
+    echo "NGINX Configuration Updated"
+fi
