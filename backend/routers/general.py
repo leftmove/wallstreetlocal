2fbac6a0230d9c9f4b88ee8a64ee65a6e87d76c9
bruhbruhroblox <100anonyo@gmail.com>
2023-09-03 16:18:29 -0400

    cached general routes

--- backend/routers/general.py
+++ backend/routers/general.py
@@ -1,22 +1,27 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 
+from .utils.cache import *
+
 router = APIRouter(
     tags=["general"],
     responses={},
 )
 
 
+@cache(168)
 @router.get("/", status_code=200)
 async def info():
     return {"message": "Hello World!"}
 
 
+@cache(168)
 @router.get("/undefined", status_code=200)
 async def info_undefined():
     return {"message": "Hello World!"}
 
 
+@cache(168)
 @router.get("/favicon.ico", status_code=200)
 async def favicon():
     return FileResponse("./public/favicon.ico")
