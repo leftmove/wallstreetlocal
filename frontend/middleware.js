64cff18f750794137509170d2d6efe4f348eaeb7
bruhbruhroblox <100anonyo@gmail.com>
2023-09-04 13:47:09 -0400

    csp middleware

---
+++ frontend/middleware.js
@@ -0,0 +1,33 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+    const nonce = crypto.randomUUID()
+    const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
+
+    const requestHeaders = new Headers()
+    requestHeaders.set('x-nonce', nonce)
+    requestHeaders.set(
+        'Content-Security-Policy',
+        // Replace newline characters and spaces
+        cspHeader.replace(/\s{2,}/g, ' ').trim()
+    )
+
+    return NextResponse.next({
+        headers: requestHeaders,
+        request: {
+            headers: requestHeaders,
+        },
+    })
+}
