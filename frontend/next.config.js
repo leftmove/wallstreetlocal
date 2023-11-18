9a7d918822f01e804cb81deb9a5ee8c6a68827c7
zipped <73859069+bruhbruhroblox@users.noreply.github.com>
2023-11-18 04:13:23 +0000

    cors attempt for vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -27,6 +27,27 @@ const nextConfig = {
   //   NEXT_PUBLIC_SERVER:
   //     "https://ominous-invention-j9xg5vxq4692x77-8000.app.github.dev",
   // },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   output: "standalone",
 };
 
