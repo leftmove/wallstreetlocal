fb953f9cdb8dbf135c0ed064b977d6738c6fb495
zipped <73859069+bruhbruhroblox@users.noreply.github.com>
2023-12-08 05:16:47 +0000

    finalish commit

--- frontend/components/Filer/Info.jsx
+++ frontend/components/Filer/Info.jsx
@@ -66,19 +66,27 @@ const Info = (props) => {
             expand ? styles["sub-header-expanded"] : "",
           ].join(" ")}
         >
-          <div className={styles["secondary-header"]}>
-            <span
-              className={[styles["secondary-header-desc"], font.className].join(
-                " "
-              )}
-            >
-              {info?.cik}{" "}
-              {info?.tickers.length ? `(${info?.tickers.join(", ")})` : ""}
-            </span>
-            {info?.data.description ? (
-              <Expand onClick={() => setExpand(!expand)} expandState={expand} />
-            ) : null}
-            <Source cik={cik} />
+          <div className={styles["secondary-headers"]}>
+            <div className={styles["secondary-header"]}>
+              <span
+                className={[
+                  styles["secondary-header-desc"],
+                  font.className,
+                ].join(" ")}
+              >
+                {info?.cik}{" "}
+                {info?.tickers.length ? `(${info?.tickers.join(", ")})` : ""}
+              </span>
+            </div>
+            <div className={styles["secondary-header"]}>
+              {info?.data.description ? (
+                <Expand
+                  onClick={() => setExpand(!expand)}
+                  expandState={expand}
+                />
+              ) : null}
+              <Source cik={cik} />
+            </div>
           </div>
           <span className={[styles["header-desc"], font.className].join(" ")}>
             {info?.data?.description}
