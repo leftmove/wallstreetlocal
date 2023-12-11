a2b3bb4e00a1c9f7458ac3559ef9d401b138527a
zipped <73859069+bruhbruhroblox@users.noreply.github.com>
2023-12-11 15:36:23 +0000

    speed insights

--- frontend/components/Layouts/Home.jsx
+++ frontend/components/Layouts/Home.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Layout({ children }) {
   return (
@@ -9,6 +10,7 @@ export default function Layout({ children }) {
       <Navbar variant="home" />
       <div>{children}</div>
       <Analytics />
+      <SpeedInsights />
       <Footer />
     </>
   );
