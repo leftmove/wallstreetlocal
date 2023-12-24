c841993f3186404aaa1af61ea421a1bdd95b93e4
chrisgarcia4158 <90802231+chrisgarcia4158@users.noreply.github.com>
2023-12-24 03:36:25 +0000

    moved loading bar to nav

--- frontend/components/Layouts/Home.jsx
+++ frontend/components/Layouts/Home.jsx
@@ -1,13 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-import Bar from "@/components/Bar/Bar";
 
 import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Bar />
       <Navbar variant="home" />
       <div>{children}</div>
       <Analytics />
--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -4,56 +4,60 @@ import Link from "next/link";
 import { font } from "@fonts";
 
 import Search from "@/components/Search/Button/Search";
+import Bar from "@/components/Bar/Bar";
 
 const Navbar = (props) => {
   const variant = props.variant || null;
   return (
-    <nav className={styles["nav"]}>
-      <div className={styles["logo"]}>
-        <div className={styles["logo-title"]}>
-          <Link href="/">
-            <span
-              className={styles["logo-text"] + " " + font.className}
-              id={styles["whale"]}
-            >
-              <i>wallstreet</i>
-            </span>
-            <span
-              className={styles["logo-text"] + " " + font.className}
-              id={styles["market"]}
-            >
-              {" "}
-              local
-            </span>
-          </Link>
+    <>
+      <Bar />
+      <nav className={styles["nav"]}>
+        <div className={styles["logo"]}>
+          <div className={styles["logo-title"]}>
+            <Link href="/">
+              <span
+                className={styles["logo-text"] + " " + font.className}
+                id={styles["whale"]}
+              >
+                <i>wallstreet</i>
+              </span>
+              <span
+                className={styles["logo-text"] + " " + font.className}
+                id={styles["market"]}
+              >
+                {" "}
+                local
+              </span>
+            </Link>
+          </div>
+          {variant === "home" ? null : <Search />}
         </div>
-        {variant === "home" ? null : <Search />}
-      </div>
-      <div>
-        <ul className={styles["about"]}>
-          <li className={styles["item"] + " " + font.className}>
-            <Link href="/recommended/top">Top Filers</Link>
-          </li>
-          <li className={styles["item"] + " " + font.className}>
-            <Link href="/recommended/searched">Popular Filers</Link>
-          </li>
-          {/* <li className={styles["item"] + " " + font.className}>
+        <div>
+          <ul className={styles["about"]}>
+            <li className={styles["item"] + " " + font.className}>
+              <Link href="/recommended/top">Top Filers</Link>
+            </li>
+            <li className={styles["item"] + " " + font.className}>
+              <Link href="/recommended/searched">Popular Filers</Link>
+            </li>
+            {/* <li className={styles["item"] + " " + font.className}>
           <Link href="/">about</Link>
         </li> */}
-          <li className={styles["item"] + " " + font.className}>
-            <Link href="/about/resources">Resources</Link>
-          </li>
-          {/* <li className={styles["item"] + " " + font.className}>
+            <li className={styles["item"] + " " + font.className}>
+              <Link href="/about/resources">Resources</Link>
+            </li>
+            {/* <li className={styles["item"] + " " + font.className}>
           <Link href="/">pricing</Link>
         </li> */}
-          <li className={styles["item"] + " " + font.className}>
-            <Link href="https://github.com/bruhbruhroblox" target="_blank">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <li className={styles["item"] + " " + font.className}>
+              <Link href="https://github.com/bruhbruhroblox" target="_blank">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
