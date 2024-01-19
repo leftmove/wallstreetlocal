00034098f73f3cfb2dc37fb35d52ae9a7a9f5952
zipped <73859069+bruhbruhroblox@users.noreply.github.com>
2024-01-19 23:43:38 +0000

    removed .next

--- .next/standalone/node_modules/@dnd-kit/utilities/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./utilities.cjs.production.min.js')
-} else {
-  module.exports = require('./utilities.cjs.development.js')
-}
