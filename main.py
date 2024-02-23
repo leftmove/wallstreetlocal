fe4fd0a6c3d00617f6428db35dbced92fde2ba32
chrisgarcia4158 <90802231+chrisgarcia4158@users.noreply.github.com>
2024-02-23 05:00:01 +0000

    added telemetry

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 
+# from opentelemetry import trace
+# from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+# from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+# from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+# from opentelemetry.sdk.trace import TracerProvider
+# from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
 from dotenv import load_dotenv
 
 load_dotenv(".env")
@@ -19,3 +26,12 @@ app = FastAPI(middleware=middleware)
 app.include_router(general.router)
 app.include_router(filer.router)
 app.include_router(stocks.router)
+
+# FastAPIInstrumentor.instrument_app(app)
+# resource = Resource(attributes={
+#     SERVICE_NAME: "fastapi"
+#   })
+# provider = TracerProvider(resource=resource)
+# processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://host.docker.internal:4317"))
+# provider.add_span_processor(processor)
+# trace.set_tracer_provider(provider)
