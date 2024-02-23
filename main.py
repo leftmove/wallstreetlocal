a0f74ea3a67a35410e2dda8a76eddf038bddd3f6
chrisgarcia4158 <90802231+chrisgarcia4158@users.noreply.github.com>
2024-02-23 06:13:30 +0000

    added instrumentation specifics

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 
+from opentelemetry import trace
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from dotenv import load_dotenv
 
+environment = os.environ["ENVIRONMENT"]
+endpoint = os.environ["TEL_EXPORTER_OTLP_ENDPOINT"]
 load_dotenv(".env")
 
 from routers import general
@@ -22,4 +29,12 @@ app.include_router(general.router)
 app.include_router(filer.router)
 app.include_router(stocks.router)
 
-FastAPIInstrumentor.instrument_app(app)
+if environment == "production":
+    FastAPIInstrumentor.instrument_app(app)
+    resource = Resource(attributes={
+        SERVICE_NAME: "fastapi"
+    })
+    provider = TracerProvider(resource=resource)
+    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint=endpoint))
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
