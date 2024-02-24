777442d5fd59a5805d806964b63a1d49f5ad3bc4
chrisgarcia4158 <90802231+chrisgarcia4158@users.noreply.github.com>
2024-02-24 19:45:30 +0000

    updated telemetry

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ import os
 import logging
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv(".env")
-
 from routers import general
 from routers import filer
 from routers import stocks
@@ -24,17 +20,22 @@ from routers.utils import (
 
 APP_NAME = os.environ.get("APP_NAME", "backend")
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
+HOST = os.environ.get("HOST", "0.0.0.0")
 EXPOSE_PORT = int(os.environ.get("EXPOSE_PORT", 8000))
 FORWARDED_ALLOW_IPS = os.environ.get("FORWARDED_ALLOW_IPS", "*")
 WORKERS = int(os.environ.get("WORKERS", "9" if ENVIRONMENT == "production" else "1"))
 OTLP_GRPC_ENDPOINT = os.environ.get("OTLP_GRPC_ENDPOINT", "http://trace:4317")
+production_environment = True if ENVIRONMENT == "production" else False
 
 middleware = [
-    Middleware(PrometheusMiddleware, app_name=APP_NAME),
     Middleware(
         CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
     ),
 ]
+if production_environment:
+    middleware.append(
+        Middleware(PrometheusMiddleware, app_name=APP_NAME),
+    )
 
 app = FastAPI(middleware=middleware)
 app.include_router(general.router)
@@ -51,11 +52,11 @@ if __name__ == "__main__":
         "fmt"
     ] = "%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s] - %(message)s"
 
-    if ENVIRONMENT == "production":
+    if production_environment:
         setting_otlp(app, APP_NAME, OTLP_GRPC_ENDPOINT)
         uvicorn.run(
             app,
-            host="0.0.0.0",
+            host=HOST,
             port=EXPOSE_PORT,
             log_config=log_config,
             forwarded_allow_ips=FORWARDED_ALLOW_IPS,
@@ -64,7 +65,7 @@ if __name__ == "__main__":
     else:
         uvicorn.run(
             app,
-            host="0.0.0.0",
+            host=HOST,
             port=EXPOSE_PORT,
             log_config=log_config,
             forwarded_allow_ips=FORWARDED_ALLOW_IPS,
