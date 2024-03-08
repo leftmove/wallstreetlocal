08fa10ae271e706043e722bb4d15681c40ccd58e
pavillion99 <145094042+pavillion99@users.noreply.github.com>
2024-03-08 18:00:06 +0000

    removed is mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,11 @@
 import "@/styles/globals.css";
 
 import Layout from "@/components/Layouts/Layout";
-import MobileLayout from "@/components/Layouts/Mobile";
-
-import { isMobile } from "react-device-detect";
-
-// function App({ ComponenpageProps, ...rest }) {
-//   const customLayout = Component.getLayout
-//     ? () => Component.getLayout(<Component {...pageProps} />)
-//     : null;
-//   const reduxLayout = Component.reduxLayout
-//     ? () => {
-//         const { store, props } = wrapper.useWrappedStore(rest);
-//         return (
-//           <Layout>
-//             <Provider store={store}>
-//               {<Component {...props.pageProps} />}
-//             </Provider>
-//           </Layout>
-//         );
-//       }
-//     : null;
-//   const defaultLayout = () => <Layout>{<Component {...pageProps} />}</Layout>;
-
-//   if (customLayout) return customLayout();
-//   if (reduxLayout) return reduxLayout();
-//   return defaultLayout();
-// }
-
-// export default App;
 
 function App(props) {
   const getLayout =
     props.Component.getLayout ||
     (() => <Layout>{<props.Component {...props.pageProps} />}</Layout>);
-  if (isMobile) {
-    return <MobileLayout />;
-  }
 
   return getLayout(props);
 }
