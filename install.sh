d9f88e9bacb39d116c4ad890fa649708633d69ae
chrisgarcia4158 <90802231+chrisgarcia4158@users.noreply.github.com>
2024-03-10 01:18:13 +0000

    update top.json

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 sudo docker system prune -a -f
-sudo docker compose -f docker-compose.yml down
 
-sudo docker compose -f docker-compose.yml up --build -d
+sudo docker compose -f docker-compose.yml up --force-recreate --build -d backend
