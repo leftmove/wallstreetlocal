e5d3c325b725debd9219348172ce59ae873fe0e5
zipped <100anonyo@gmail.com>
2024-03-16 22:34:30 +0000

    chart precommit

--- components/fonts/index.js
+++ components/fonts/index.js
@@ -1,15 +1,4 @@
-import localFont from "@next/font/local";
 import { Inter } from "@next/font/google";
-
-// const font = localFont({
-//   src: [
-//     { path: "./Satoshi-Variable.woff2", weight: "900", subsets: ["latin"] },
-//   ],
-// });
-// const fontBold = localFont({
-//   src: [{ path: "./Satoshi-Black.woff2" }],
-// });
-// const fontLight = localFont({ src: [{ path: "./Satoshi-Bold.woff2" }] });
 
 const font = Inter({ weight: "800", subsets: ["latin"] });
 const fontBold = Inter({ weight: "900", subsets: ["latin"] });
