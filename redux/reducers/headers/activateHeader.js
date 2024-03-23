00c500788fc6d6b7d1d9ca7436e2ebe426c2d0e5
zipped <73859069+bruhbruhroblox@users.noreply.github.com>
2024-03-23 02:58:03 +0000

    removed reducers folder

--- redux/reducers/headers/activateHeader.js
+++
@@ -1,9 +0,0 @@
-export default function activateHeader(state, action) {
-  const headers = state.headers;
-  const payload = action.payload;
-  state.headers = headers.map((h) =>
-    h.accessor === payload ? { ...h, active: !h.active } : h
-  );
-
-  return state;
-}
