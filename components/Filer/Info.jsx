5a865a6dbeca65162a45535cb7d0ca026720ade4
A Noor <100anonyo@gmail.com>
2024-05-03 08:19:28 -0400

    update convertTitle

--- components/Filer/Info.jsx
+++ components/Filer/Info.jsx
@@ -11,19 +11,26 @@ import Explorer from "components/Explorer/Explorer";
 import Header from "components/Header/Header";
 
 const convertTitle = (d) => {
-  if (d) {
-    d = d.replace(
-      /(^\w|\s\w)(\S*)/g,
-      (_, m1, m2) => m1.toUpperCase() + m2.toLowerCase()
-    );
-    ["LLC", "LP", "L.P.", "LLP"].forEach((word) => {
-      d = d.replace(
-        word.at(0).toUpperCase() + word.toLowerCase().slice(1),
-        word
-      );
-    });
-  }
-  return d;
+    if (d) {
+        d = d.replace(
+            /(^\w|\s\w)(\S*)/g,
+            (_, m1, m2) => {
+                // Check if the word is already in mixed case
+                if (/[a-z][A-Z]|[A-Z][a-z]/.test(m1 + m2)) {
+                    return m1 + m2; // Return the original word
+                } else {
+                    return m1.toUpperCase() + m2.toLowerCase(); // Convert to title case
+                }
+            }
+        );
+        ["LLC", "LP", "L.P.", "LLP"].forEach((word) => {
+            d = d.replace(
+                word.at(0).toUpperCase() + word.toLowerCase().slice(1),
+                word
+            );
+        });
+    }
+    return d;
 };
 
 const Info = (props) => {
