32f1dbff605c29feff5231ae2207d198c2d7b1a1
pavillion 99 <leadershipprints@gmail.com>
2024-05-04 00:57:17 +0000

    query

--- routers/worker.py
+++ routers/worker.py
@@ -1,5 +1,6 @@
 import os
 import multiprocessing
+from dotenv import load_dotenv
 
 from celery import Celery, signals
 
@@ -8,6 +9,8 @@ from sentry_sdk.integrations.celery import CeleryIntegration
 
 from . import filer
 
+load_dotenv()
+
 REDIS_SERVER_URL = os.environ.get("REDIS_SERVER_URL", "cache")
 REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
 BROKER = f"redis://{REDIS_SERVER_URL}:{REDIS_PORT}/0"
@@ -15,9 +18,9 @@ SENTRY_DSN = os.environ.get("SENTRY_DSN", "")
 WORKERS = int(os.environ.get("WORKERS", ((multiprocessing.cpu_count() * 2) + 1)))
 TELEMETRY = bool(os.environ.get("TELEMETRY", False))
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "development")
+
 production_environment = True if ENVIRONMENT == "production" else False
 run_telemetry = True if TELEMETRY else False
-
 config = {"worker_concurrency": WORKERS, "broker_connection_retry_on_startup": True}
 
 queue = Celery("worker", broker=BROKER)
