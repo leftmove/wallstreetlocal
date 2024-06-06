ec81d7cb19b5c59de374841724dba5a8179d4ffc
Anonyo Noor <100anonyo@gmail.com>
2024-06-06 15:51:30 +0000

    removed allocation

--- frontend/components/Charts/Allocation/Allocation.tsx
+++ frontend/components/Charts/Allocation/Allocation.tsx
@@ -70,146 +70,150 @@ const Allocation: React.FC<AllocationProps> = ({
   events = false,
   margin = defaultMargin,
 }) => {
-  const {
-    tooltipOpen,
-    tooltipLeft,
-    tooltipTop,
-    tooltipData,
-    hideTooltip,
-    showTooltip,
-  } = useTooltip();
 
-  const { containerRef, TooltipInPortal } = useTooltipInPortal({
-    // TooltipInPortal is rendered in a separate child of <body /> and positioned
-    // with page coordinates which should be updated on scroll. consider using
-    // Tooltip or TooltipWithBounds if you don't need to render inside a Portal
-    scroll: true,
-  });
+  return <></>
 
-  if (width < 10) return null;
-  // bounds
-  const xMax = width;
-  const yMax = height - margin.top - 100;
+  // const {
+  //   tooltipOpen,
+  //   tooltipLeft,
+  //   tooltipTop,
+  //   tooltipData,
+  //   hideTooltip,
+  //   showTooltip,
+  // } = useTooltip();
 
-  dateScale.rangeRound([0, xMax]);
-  temperatureScale.range([yMax, 0]);
-  console.log(cityTemperature);
+  // const { containerRef, TooltipInPortal } = useTooltipInPortal({
+  //   // TooltipInPortal is rendered in a separate child of <body /> and positioned
+  //   // with page coordinates which should be updated on scroll. consider using
+  //   // Tooltip or TooltipWithBounds if you don't need to render inside a Portal
+  //   scroll: true,
+  // });
 
-  return width < 10 ? null : (
-    <div style={{ position: "relative" }}>
-      <svg ref={containerRef} width={width} height={height}>
-        <rect
-          x={0}
-          y={0}
-          width={width}
-          height={height}
-          fill={background}
-          rx={14}
-        />
-        <Grid
-          top={margin.top}
-          left={margin.left}
-          xScale={dateScale}
-          yScale={temperatureScale}
-          width={xMax}
-          height={yMax}
-          stroke="black"
-          strokeOpacity={0.1}
-          xOffset={dateScale.bandwidth() / 2}
-        />
-        <Group top={margin.top}>
-          <BarStack
-            data={data}
-            keys={keys}
-            x={getDate}
-            xScale={dateScale}
-            yScale={temperatureScale}
-            color={colorScale}
-          >
-            {(barStacks) =>
-              barStacks.map((barStack) =>
-                barStack.bars.map((bar) => (
-                  <rect
-                    key={`bar-stack-${barStack.index}-${bar.index}`}
-                    x={bar.x}
-                    y={bar.y}
-                    height={bar.height}
-                    width={bar.width}
-                    fill={bar.color}
-                    onClick={() => {
-                      if (events) alert(`clicked: ${JSON.stringify(bar)}`);
-                    }}
-                    onMouseLeave={() => {
-                      tooltipTimeout = window.setTimeout(() => {
-                        hideTooltip();
-                      }, 300);
-                    }}
-                    onMouseMove={(event) => {
-                      if (tooltipTimeout) clearTimeout(tooltipTimeout);
-                      // TooltipInPortal expects coordinates to be relative to containerRef
-                      // localPoint returns coordinates relative to the nearest SVG, which
-                      // is what containerRef is set to in this example.
-                      const eventSvgCoords = localPoint(event);
-                      const left = bar.x + bar.width / 2;
-                      showTooltip({
-                        tooltipData: bar,
-                        tooltipTop: eventSvgCoords?.y,
-                        tooltipLeft: left,
-                      });
-                    }}
-                  />
-                ))
-              )
-            }
-          </BarStack>
-        </Group>
-        <AxisBottom
-          top={yMax + margin.top}
-          scale={dateScale}
-          tickFormat={formatDate}
-          stroke={purple3}
-          tickStroke={purple3}
-          tickLabelProps={{
-            fill: purple3,
-            fontSize: 11,
-            textAnchor: "middle",
-          }}
-        />
-      </svg>
-      <div
-        style={{
-          position: "absolute",
-          top: margin.top / 2 - 10,
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          fontSize: "14px",
-        }}
-      >
-        <LegendOrdinal
-          scale={colorScale}
-          direction="row"
-          labelMargin="0 15px 0 0"
-        />
-      </div>
+  // if (width < 10) return null;
+  // // bounds
+  // const xMax = width;
+  // const yMax = height - margin.top - 100;
 
-      {tooltipOpen && tooltipData && (
-        <TooltipInPortal
-          top={tooltipTop}
-          left={tooltipLeft}
-          style={tooltipStyles}
-        >
-          <div style={{ color: colorScale(tooltipData.key) }}>
-            <strong>{tooltipData.key}</strong>
-          </div>
-          <div>{tooltipData.bar.data[tooltipData.key]}℉</div>
-          <div>
-            <small>{formatDate(getDate(tooltipData.bar.data))}</small>
-          </div>
-        </TooltipInPortal>
-      )}
-    </div>
-  );
+  // dateScale.rangeRound([0, xMax]);
+  // temperatureScale.range([yMax, 0]);
+  // console.log(cityTemperature);
+
+  // return width < 10 ? null : (
+  //   <div style={{ position: "relative" }}>
+  //     <svg ref={containerRef} width={width} height={height}>
+  //       <rect
+  //         x={0}
+  //         y={0}
+  //         width={width}
+  //         height={height}
+  //         fill={background}
+  //         rx={14}
+  //       />
+  //       <Grid
+  //         top={margin.top}
+  //         left={margin.left}
+  //         xScale={dateScale}
+  //         yScale={temperatureScale}
+  //         width={xMax}
+  //         height={yMax}
+  //         stroke="black"
+  //         strokeOpacity={0.1}
+  //         xOffset={dateScale.bandwidth() / 2}
+  //       />
+  //       <Group top={margin.top}>
+  //         <BarStack
+  //           data={data}
+  //           keys={keys}
+  //           x={getDate}
+  //           xScale={dateScale}
+  //           yScale={temperatureScale}
+  //           color={colorScale}
+  //         >
+  //           {(barStacks) =>
+  //             barStacks.map((barStack) =>
+  //               barStack.bars.map((bar) => (
+  //                 <rect
+  //                   key={`bar-stack-${barStack.index}-${bar.index}`}
+  //                   x={bar.x}
+  //                   y={bar.y}
+  //                   height={bar.height}
+  //                   width={bar.width}
+  //                   fill={bar.color}
+  //                   onClick={() => {
+  //                     if (events) alert(`clicked: ${JSON.stringify(bar)}`);
+  //                   }}
+  //                   onMouseLeave={() => {
+  //                     tooltipTimeout = window.setTimeout(() => {
+  //                       hideTooltip();
+  //                     }, 300);
+  //                   }}
+  //                   onMouseMove={(event) => {
+  //                     if (tooltipTimeout) clearTimeout(tooltipTimeout);
+  //                     // TooltipInPortal expects coordinates to be relative to containerRef
+  //                     // localPoint returns coordinates relative to the nearest SVG, which
+  //                     // is what containerRef is set to in this example.
+  //                     const eventSvgCoords = localPoint(event);
+  //                     const left = bar.x + bar.width / 2;
+  //                     showTooltip({
+  //                       tooltipData: bar,
+  //                       tooltipTop: eventSvgCoords?.y,
+  //                       tooltipLeft: left,
+  //                     });
+  //                   }}
+  //                 />
+  //               ))
+  //             )
+  //           }
+  //         </BarStack>
+  //       </Group>
+  //       <AxisBottom
+  //         top={yMax + margin.top}
+  //         scale={dateScale}
+  //         tickFormat={formatDate}
+  //         stroke={purple3}
+  //         tickStroke={purple3}
+  //         tickLabelProps={{
+  //           fill: purple3,
+  //           fontSize: 11,
+  //           textAnchor: "middle",
+  //         }}
+  //       />
+  //     </svg>
+  //     <div
+  //       style={{
+  //         position: "absolute",
+  //         top: margin.top / 2 - 10,
+  //         width: "100%",
+  //         display: "flex",
+  //         justifyContent: "center",
+  //         fontSize: "14px",
+  //       }}
+  //     >
+  //       <LegendOrdinal
+  //         scale={colorScale}
+  //         direction="row"
+  //         labelMargin="0 15px 0 0"
+  //       />
+  //     </div>
+
+  //     {tooltipOpen && tooltipData && (
+  //       <TooltipInPortal
+  //         top={tooltipTop}
+  //         left={tooltipLeft}
+  //         style={tooltipStyles}
+  //       >
+  //         <div style={{ color: colorScale(tooltipData.key) }}>
+  //           <strong>{tooltipData.key}</strong>
+  //         </div>
+  //         <div>{tooltipData.bar.data[tooltipData.key]}℉</div>
+  //         <div>
+  //           <small>{formatDate(getDate(tooltipData.bar.data))}</small>
+  //         </div>
+  //       </TooltipInPortal>
+  //     )}
+  //   </div>
+  // );
+  
 };
 
 export default Allocation;
