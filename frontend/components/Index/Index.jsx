a6d04b6cc8b74a0308bed0a90e46b83244ef84ae
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-06 15:21:23 +0000

    edit_file

--- frontend/components/Index/Index.jsx
+++ frontend/components/Index/Index.jsx
@@ -1,10 +1,7 @@
 import styles from "./Index.module.css";
 import { useEffect, useState } from "react";
-
 import axios from "axios";
-
 import Error from "next/error";
-
 import { useDispatch, useSelector } from "react-redux";
 import {
   setStocks,
@@ -18,17 +15,25 @@ import {
   selectSort,
   selectHeaders,
 } from "@/redux/filerSlice";
-
 import useStocks from "components/Hooks/useStocks";
 import Analysis from "components/Analysis/Analysis";
 import Table from "components/Table/Table";
 import Sort from "./Sort/Sort";
 
-const server = process.env.NEXT_PUBLIC_SERVER;
-const Index = () => {
+const server: string | undefined = process.env.NEXT_PUBLIC_SERVER;
+
+interface Stock {
+  // Define the properties of a stock item here
+}
+
+interface Pagination {
+  // Define the properties of pagination here
+}
+
+const Index: React.FC = () => {
   const dispatch = useDispatch();
-  const cik = useSelector(selectCik);
-  const stocks = useSelector(selectStocks);
+  const cik: string = useSelector(selectCik);
+  const stocks: Stock[] = useSelector(selectStocks);
 
   const {
     items,
@@ -47,19 +52,19 @@ const Index = () => {
     useSelector(selectPagination),
     useSelector(selectSort),
     useSelector(selectStocks),
-    (s) => dispatch(setCount(s)),
-    (c) => dispatch(setStocks(c)),
-    (a, d) =>
+    (s: number) => dispatch(setCount(s)),
+    (c: Stock[]) => dispatch(setStocks(c)),
+    (a: string, d: boolean) =>
       dispatch(
         sortHeader({
           sort: a,
           reverse: d,
         })
       ),
-    (o) => {
+    (o: number) => {
       dispatch(setOffset(o));
     },
-    (p) => {
+    (p: Pagination) => {
       dispatch(setPagination(p));
     }
   );
