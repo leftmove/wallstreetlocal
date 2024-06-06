4d99272e5b775636ddce82787f632d20df8c38aa
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-06 15:20:46 +0000

    edit_file

--- frontend/components/Filer/Other.jsx
+++ frontend/components/Filer/Other.jsx
@@ -1,15 +1,17 @@
 import Head from "next/head";
 import Error from "next/error";
-
-const Other = () => {
+import { FC } from "react";
+enum StatusCode {
+  NotFound = 404,
+}
+const Other: FC = () => {
   return (
     <>
       <Head>
         <title>404 - Filer not found</title>
       </Head>
-      <Error statusCode={404} />
+      <Error statusCode={StatusCode.NotFound} />
     </>
   );
 };
-
 export default Other;
