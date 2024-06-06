3805e94222d13eb4c72bda491681446a01aba1d5
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-06 15:17:44 +0000

    edit_file

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,9 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { createWrapper } from "next-redux-wrapper";
-
+import { configureStore, EnhancedStore } from "@reduxjs/toolkit";
+import { createWrapper, Context, MakeStore } from "next-redux-wrapper";
+import { AnyAction, MiddlewareArray } from "@reduxjs/toolkit";
 import { filerSlice } from "./filerSlice";
-
-const makeStore = () =>
+interface StoreState {
+  [key: string]: any;
+}
+const makeStore: MakeStore<EnhancedStore<StoreState, AnyAction>> = (context: Context) =>
   configureStore({
     reducer: {
       [filerSlice.name]: filerSlice.reducer,
@@ -11,8 +13,7 @@ const makeStore = () =>
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
         serializableCheck: false,
-      }),
+      }) as MiddlewareArray,
     devTools: true,
   });
-
-export const wrapper = createWrapper(makeStore);
+export const wrapper = createWrapper<EnhancedStore<StoreState, AnyAction>>(makeStore);
