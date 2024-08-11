2357ab4f156bb7bcb071a55b405958563e4cb54b
leftmove <100anonyo@gmail.com>
2024-08-11 15:26:58 -0400

    maintenance

---
+++ frontend/components/Maintenance/Maintenance.jsx
@@ -0,0 +1,13 @@
+import styles from "./Maintenance.module.css";
+
+const Maintenance = () => {
+  return (
+    <div className={styles["container"]}>
+      <h1 className={styles["text"]}>
+        wallstreetlocal is undergoing maintenance, please try again later.
+      </h1>
+    </div>
+  );
+};
+
+export default Maintenance;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
 import Layout from "components/Layouts/Layout";
+import Maintenance from "components/Maintenance/Maintenance";
 
 import { Inter } from "@next/font/google";
 
@@ -9,10 +10,10 @@ const fontBold = Inter({ weight: "900", subsets: ["latin"] });
 const fontLight = Inter({ weight: "700", subsets: ["latin"] });
 
 function App(props) {
+  return <Maintenance />;
   const getLayout =
     props.Component.getLayout ||
     (() => <Layout>{<props.Component {...props.pageProps} />}</Layout>);
-
   return getLayout(props);
 }
 
