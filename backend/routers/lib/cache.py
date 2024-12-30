ad00e330b73109d440da4c74dda46eccaeb5eaf5
leftmove <100anonyo@gmail.com>
2024-12-30 01:04:17 -0500

    na fix

--- backend/routers/lib/cache.py
+++ backend/routers/lib/cache.py
@@ -16,7 +16,7 @@ production_environment = True if ENVIRONMENT == "production" else False
 
 REDIS_SERVER_URL = os.environ.get("REDIS_SERVER_URL", "cache")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
-REDIS_USERN/AME = os.environ.get("REDIS_USERN/AME", "default")
+REDIS_USERNAME = os.environ.get("REDIS_USERNAME", "default")
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "")
 
 store = redis.Redis(
