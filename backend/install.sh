287678bf8983002356f941ec18f69b541433923e
leftmove <100anonyo@gmail.com>
2025-01-13 03:09:17 -0500

    redis clean on start

--- backend/install.sh
+++ backend/install.sh
@@ -1,5 +1,6 @@
 sudo systemctl stop uvicorn
 sudo systemctl stop celery
 uv run python -m celery -A worker.tasks.queue purge -f
+redis-cli flushall
 sudo systemctl restart celery
 sudo systemctl start uvicorn
