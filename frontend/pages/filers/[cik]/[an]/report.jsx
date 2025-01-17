181919cb61b9cf25d86369eb6db21b3256960f31
leftmove <100anonyo@gmail.com>
2025-01-17 12:54:09 -0500

    fixed build error for front-end

---
+++ frontend/pages/filers/[cik]/[an]/report.jsx
@@ -0,0 +1,14 @@
+const Report = () => {
+  return (
+    <div>
+      <h1>Report Page</h1>
+      <p>
+        This page is meant for a planned 10K report feature, not yet
+        implemented.
+      </p>
+      <p>Check back later.</p>
+    </div>
+  );
+};
+
+export default Report;
